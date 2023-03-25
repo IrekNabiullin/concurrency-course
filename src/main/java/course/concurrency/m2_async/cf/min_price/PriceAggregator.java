cd50b629ac80c7a2b8a6655d745aeb228eb1cd0e
IrekNabiullin <08git08@mail.ru>
2023-03-25 16:09:51 +0300

    TASK2-3 PriceAggregator

--- src/main/java/course/concurrency/m2_async/cf/min_price/PriceAggregator.java
+++ src/main/java/course/concurrency/m2_async/cf/min_price/PriceAggregator.java
@@ -8,7 +8,7 @@ import java.util.concurrent.*;
 import static java.util.concurrent.TimeUnit.SECONDS;
 
 public class PriceAggregator {
-
+    ExecutorService executor = Executors.newCachedThreadPool();
     private PriceRetriever priceRetriever = new PriceRetriever();
 
     public void setPriceRetriever(PriceRetriever priceRetriever) {
@@ -23,67 +23,49 @@ public class PriceAggregator {
 
     public double getMinPrice(long itemId) {
         // здесь будет ваш код
-        double result;
         List<Double> prices = new ArrayList<>();
-        ExecutorService executor = Executors.newCachedThreadPool();
-
-        CompletableFuture<List<Double>> pricesResult = CompletableFuture.supplyAsync(() -> {
-            for (long shopId : shopIds) {
-                executor.submit(() -> {
-                    try {
-                        Double currentPrice = CompletableFuture
-                                .supplyAsync(() -> priceRetriever.getPrice(itemId, shopId), executor)
-                                .handle((price, ex) -> {
-                            if (ex != null) {
-                                return Double.NaN;
-                            } else {
-                                return price;
-                            }
-                        }).get(3, SECONDS);
-
-                        if(!currentPrice.equals(Double.NaN)) {
-                            prices.add(currentPrice);
-                        }
-                    } catch (InterruptedException | ExecutionException | TimeoutException e) {
-                        e.printStackTrace();
-                        throw new RuntimeException(e);
-                    }
-                    return prices;
-                });
-            }
-
-            try {
-                Thread.sleep(2500);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-
-            return prices;
-        });
-
-        Callable <Double>  minDoublePrice = () -> {
-            double minPrice = 0;
-            try {
-                minPrice = pricesResult.thenApply((allPrices) -> {
-                    if (allPrices.size() == 0) {
-                        return Double.NaN;
-                    } else {
-                        return allPrices.stream().reduce(Double::min).get();
-                    }
-                }).get();
-            } catch (InterruptedException | ExecutionException e) {
-                e.printStackTrace();
-                throw new RuntimeException(e);
-            }
-            return minPrice;
-        };
 
         try {
-            result = minDoublePrice.call();
-        } catch (Exception e) {
-            e.printStackTrace();
+            return CompletableFuture.supplyAsync(() -> {
+                for (long shopId : shopIds) {
+                    executor.submit(() -> {
+                        try {
+                            Double currentPrice = CompletableFuture
+                                    .supplyAsync(() -> priceRetriever.getPrice(itemId, shopId), executor)
+                                    .handle((price, ex) -> {
+                                        if (ex != null) {
+                                            return Double.NaN;
+                                        } else {
+                                            return price;
+                                        }
+                                    }).get(3, SECONDS);
+
+                            if (!currentPrice.equals(Double.NaN)) {
+                                prices.add(currentPrice);
+                            }
+                        } catch (InterruptedException | ExecutionException | TimeoutException e) {
+                            throw new RuntimeException(e);
+                        }
+                        return prices;
+                    });
+                }
+
+                try {
+                    Thread.sleep(2500);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+
+                return prices;
+            }).thenApply((allPrices) -> {
+                if (allPrices.size() == 0) {
+                    return Double.NaN;
+                } else {
+                    return allPrices.stream().reduce(Double::min).get();
+                }
+            }).get();
+        } catch (InterruptedException | ExecutionException e) {
             throw new RuntimeException(e);
         }
-        return result;
     }
 }
