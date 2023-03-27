4ffc64fbb33453a5cf98f410b2e39451f20c04bb
IrekNabiullin <08git08@mail.ru>
2023-03-27 04:04:15 +0300

    TASK2-3 PriceAggregator ver4 all tests ok

--- src/main/java/course/concurrency/m2_async/cf/min_price/PriceAggregator.java
+++ src/main/java/course/concurrency/m2_async/cf/min_price/PriceAggregator.java
@@ -7,6 +7,7 @@ import java.util.*;
 import java.util.concurrent.*;
 import java.util.stream.Stream;
 
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
 import static java.util.concurrent.TimeUnit.SECONDS;
 
 public class PriceAggregator {
@@ -25,167 +26,22 @@ public class PriceAggregator {
 
     public double getMinPrice(long itemId) {
         // здесь будет ваш код
-//        Double result = Double.NaN;
-        long startTime = System.currentTimeMillis();
-        System.out.println("start time = " + startTime);
-        List<Double> prices = new ArrayList<>();
-
-        System.out.println("1");
-
         CompletableFuture[] futures = shopIds.stream()
-                .map(shopId -> {
-                    CompletableFuture f = CompletableFuture
-                            .supplyAsync(() -> priceRetriever.getPrice(itemId, shopId), executor)
-                            .handle((price, ex) -> {
-                                if (ex != null) {
-                                    System.out.println("Thread: " + Thread.currentThread().getName() + " shopId = " + shopId + "return Double.NaN");
-                                    return Double.NaN;
-                                } else {
-                                    if (price > 0) {
-                                        prices.add(price);
-                                    }
-                                    long currentTimeMillis = System.currentTimeMillis();
-                                    System.out.println("Thread: " + Thread.currentThread().getName() + " shopId = " + shopId + "price = " + price +
-                                            " currentTimeMillis = " + currentTimeMillis + " time elapsed = " + (currentTimeMillis - startTime));
-                                    return price;
-                                }
-                            });
-                    return f;
-                })
+                .map(shopId -> CompletableFuture
+                        .supplyAsync(() -> priceRetriever.getPrice(itemId, shopId), executor)
+                        .handle((price, ex) -> ex == null ? price : Double.NaN))
                 .toArray(CompletableFuture[]::new);
 
-        long currentTimeMillis = System.currentTimeMillis();
-        System.out.println("2 currentTimeMillis = " + currentTimeMillis + " time elapsed = " + (currentTimeMillis - startTime));
 
-        CompletableFuture<Void> all = CompletableFuture.allOf(futures);
-
-        currentTimeMillis = System.currentTimeMillis();
-        System.out.println("3 currentTimeMillis = " + currentTimeMillis + " time elapsed = " + (currentTimeMillis - startTime));
-
-        System.out.println("futures.length = " + futures.length);
-        System.out.println("prices.size() = " + prices.size());
-        currentTimeMillis = System.currentTimeMillis();
-        System.out.println("4 currentTimeMillis = " + currentTimeMillis + " time elapsed = " + (currentTimeMillis - startTime));
-
-
-        try {
-            return all
-                    .thenApply((prc) -> {
-                        if (prices.size() == 0) {
-//                            System.out.println("prices.size() = " + prices.size());
-//                            System.out.println("Thread: " + Thread.currentThread().getName() + " return Double.NaN;");
-                            return Double.NaN;
-                        } else {
-                            System.out.println("prices.sizzze() = " + prices.size());
-                            prices.forEach(System.out::println);
-                            return prices.stream().reduce(Double::min).get();
-//                            System.out.println("res = " + res);
-//                            return res;
-                        }
-                    }).get();
-        } catch (InterruptedException | ExecutionException e) {
-            throw new RuntimeException(e);
-        }
+        return Arrays.stream(futures)
+                .filter(Objects::nonNull)
+                .map((future) -> CompletableFuture.supplyAsync(() -> {
+                    try {
+                        return (Double) future.get(1, MILLISECONDS);
+                    } catch (InterruptedException | ExecutionException | TimeoutException e) {
+                        return Double.NaN;
+                    }
+                }, executor).join())
+                .min(Comparator.naturalOrder()).orElse(Double.NaN);
     }
-
-/*
-        try {
-            return CompletableFuture.supplyAsync(() -> {
-                        for (long shopId : shopIds) {
-                            double currentPrice;
-                            try {
-                                currentPrice = CompletableFuture.supplyAsync(() -> priceRetriever.getPrice(itemId, shopId), executor)
-                                        .handle((price, ex) -> {
-                                            if (ex != null) {
-                                                System.out.println("Thread: " + Thread.currentThread().getName() + " shopId = " + shopId + "return Double.NaN");
-                                                return Double.NaN;
-                                            } else {
-                                                System.out.println("Thread: " + Thread.currentThread().getName() + " shopId = " + shopId + "price = " + price);
-//                                                prices.add(price);
-                                                return price;
-                                            }
-                                        })
-                                        .get(3, SECONDS);
-                            } catch (InterruptedException | ExecutionException | TimeoutException ex) {
-                                throw new RuntimeException(ex);
-                            }
-                            prices.add(currentPrice);
-                        }
-                        System.out.println("prices.size = " + prices.size());
-                        return prices;
-                    })
-                    .handle((price, ex) -> {
-                        if (ex != null) {
-                            System.out.println("Thread: " + Thread.currentThread().getName() + " returning Double.NaN");
-//                                return Double.NaN;
-                        } else {
-                            System.out.println("Thread: " + Thread.currentThread().getName() + "prices.size  = " + prices.size());
-//                                return prices;
-                        }
-                        return prices;
-                    }).thenApply((prc) -> {
-                        if (prices.size() == 0) {
-                            System.out.println("Thread: " + Thread.currentThread().getName() + " return Double.NaN;");
-                            return Double.NaN;
-                        } else {
-                            Double res = prices.stream().reduce(Double::min).get();
-                            System.out.println("res = " + res);
-                            return res;
-                        }
-                    }).get(3, SECONDS);
-        } catch (InterruptedException | TimeoutException | ExecutionException e) {
-//            throw new RuntimeException(e);
-            System.out.println("Exception in 73");
-            return Double.NaN;
-        }
-    }
-
- */
-
-    /*
-        try {
-            return CompletableFuture.supplyAsync(() -> {
-                for (long shopId : shopIds) {
-                    executor.submit(() -> {
-                        try {
-                            Double currentPrice = CompletableFuture
-                                    .supplyAsync(() -> priceRetriever.getPrice(itemId, shopId), executor)
-                                    .handle((price, ex) -> {
-                                        if (ex != null) {
-                                            return Double.NaN;
-                                        } else {
-                                            return price;
-                                        }
-                                    }).get(3, SECONDS);
-
-                            if (!currentPrice.equals(Double.NaN)) {
-                                prices.add(currentPrice);
-                            }
-                        } catch (InterruptedException | ExecutionException | TimeoutException e) {
-                            throw new RuntimeException(e);
-                        }
-                        return prices;
-                    });
-                }
-
-                try {
-                    Thread.sleep(2500);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-
-                return prices;
-            }).thenApply((allPrices) -> {
-                if (allPrices.size() == 0) {
-                    return Double.NaN;
-                } else {
-                    return allPrices.stream().reduce(Double::min).get();
-                }
-            }).get();
-        } catch (InterruptedException | ExecutionException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-     */
 }
